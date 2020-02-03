64e63912743f14c9cd86bb7f98ab6ae290872c15
Filip Cernov <philipchernov95@gmail.com>
2020-02-03 17:49:33 +0000

    added explanation

--- Flash Card Creator/AppDelegate.swift
+++ Flash Card Creator/AppDelegate.swift
@@ -18,8 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //Realm Adress
         print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last as! String)
-        //Адрес  реалма
+        
         
         UITextView.appearance().tintColor = .lightGray
         
