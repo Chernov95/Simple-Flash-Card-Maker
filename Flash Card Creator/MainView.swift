56ae1f00d9bd27fa0034c57d62cd68f5002d0e04
Filip Cernov <philipchernov95@gmail.com>
2023-02-14 17:01:22 -0500

    Removes gap between create deck button and list

--- Flash Card Creator/MainView.swift
+++ Flash Card Creator/MainView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 
+//"English", "Japanese", "Latin", "Anatomy", "Grammar", "Art", "Literature", "Law", "Programming"
 struct MainView: View {
     
     @State private var decks = ["English", "Japanese", "Latin", "Anatomy", "Grammar", "Art", "Literature", "Law", "Programming"]
@@ -16,35 +17,38 @@ struct MainView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Button(action: {
-                    createDeckButtonPressed()
-                }, label: {
-                    Image("CreateDeckEnglish")
-                })
-                .frame(width: 300, height: 150)
-                HStack {
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.yellow)
-                        .padding(.leading)
-                    Text("My Decks :")
-                        .foregroundColor(.gray)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                Divider()
-                List {
-                    ForEach(decks, id: \.self) {
-                        DeckCell(deckName: $0)
+            ZStack(alignment: .top) {
+                    Button(action: {
+                        createDeckButtonPressed()
+                    }, label: {
+                        Image("CreateDeckEnglish")
+                    })
+                    .frame(width: 300, height: 130)
+                    List {
+                        Section {
+                            ForEach(decks, id: \.self) {
+                                DeckCell(deckName: $0)
+                            }
+                            .onDelete { indexSet in
+                                decks.remove(atOffsets: indexSet)
+                            }
+                            .onMove { source, destination in
+                                decks.move(fromOffsets: source, toOffset: destination)
+                            }
+                        } header: {
+                            HStack() {
+                                Image(systemName: "star.fill")
+                                    .foregroundColor(.yellow)
+                                    .padding(.leading)
+                                Text("My Decks :")
+                                    .foregroundColor(.gray)
+                            }
+                            .offset(x: -18)
+                        }
                     }
-                    .onDelete { indexSet in
-                        decks.remove(atOffsets: indexSet)
-                    }
-                    .onMove { source, destination in
-                        decks.move(fromOffsets: source, toOffset: destination)
-                    }
-                }
-                .scrollContentBackground(.hidden)
-                .listStyle(.plain)
+                    .listStyle(.plain)
+                    .scrollContentBackground(.hidden)
+                    .offset(x: 0, y: 100)
             }
             // Navigation bar styling
                 .navigationBarTitleDisplayMode(.inline)
@@ -65,7 +69,7 @@ struct MainView: View {
                 .toolbarBackground(Color(hex: "54B4F2"), for: .navigationBar)
                 .toolbarBackground(.visible, for: .navigationBar)
             
-            // Adds search bar
+            // Adds searcbar
                 .searchable(text: $searchDeck)
                 .onChange(of: searchDeck) { searchText in
                     if !searchText.isEmpty {
